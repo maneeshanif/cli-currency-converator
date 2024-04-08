f9305db9ac729d21c3db05c9354d9720969ff177
maneeshanif <manees7865@gmail.com>
2024-04-09 00:51:34 +0500

    .

---
+++ index.ts
@@ -0,0 +1,36 @@
+import inquirer from "inquirer"
+import chalk from "chalk"
+const currency: any  = {
+    USD : 1,
+    EUR : 301,
+    GBP : 351,
+    INR : 3.33,
+    PKR: 277
+};
+let userAnswer = await inquirer.prompt( 
+    [
+        {
+            name : "from",
+            message : chalk.red.bgGreenBright("\t\t\tEnter from Currency"),
+            type : "list",
+            choices :["USD","EUR" ,"GBP" ,"INR" ,"PKR"]
+         },
+         {
+            name : "to",
+            message : "Enter to Currency",
+            type : "list",
+            choices : ["USD","EUR" ,"GBP" ,"INR" ,"PKR"]
+         },
+         {
+            name : "amount",
+            message: chalk.greenBright( "Enter your amount"),
+            type : "number",
+         },
+        ]
+)
+let fromAmount = currency[userAnswer.from];
+let toAmount = currency[userAnswer.to];
+let Amount = userAnswer.amount;
+let baseAmount = Amount / fromAmount;
+let convertedAmount = baseAmount * toAmount;
+console.log(convertedAmount);
